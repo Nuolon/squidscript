c12eefe46d843e835fffee308d6c7b819717c08a
nuolon <Dryxonin@gmail.com>
2021-12-01 05:14:02 -0800

    Think it's already finished?

--- main_script.sh
+++ main_script.sh
@@ -44,19 +44,50 @@ start() {
 
 #Function to install squid package
 install_squid() {
-echo -e "${BLINKRED}yum install squid*${NC}${YEL}"
+roll "Started the installation of squid, please sit tight."
+echo -e "${BLINKRED}yum install squid* 2>&1 ${DATA_DIR}/${LOGFILE}${NC}${YEL}"
+roll "Installation finished, moving on..."
+echo -e "${LPURPLE}================================================================${NC}"
 }
 
-#Function to start squid after install
+#Function to start squid after install and add to startup
 start_squid() {
-echo -e "${RED}systemctl start squid${NC}${YEL}"
+roll "Starting squid..."
+echo -e "${RED}systemctl start squid 2>&1 ${DATA_DIR}/${LOGFILE} ${NC}${YEL}"
+echo -e "${RED}systemctl enable squid 2>&1 ${DATA_DIR}/${LOGFILE} ${NC}${YEL}"
+roll "Squid started and enabled on start-up"
+echo -e "${LPURPLE}================================================================${NC}"
+}
+
+#function to restart squid
+restart_squid() {
+roll "Restarting squid..."
+echo -e "${RED}systemctl restart squid 2>&1 ${DATA_DIR}/${LOGFILE} ${NC}${YEL}"
+roll "Squid restarted"
+echo -e "${LPURPLE}================================================================${NC}"
+}
+
+#Function to change the configuration file  of squid.
+conf_squid() {
+roll "Starting to configure squid..."
+echo -e "${RED}echo 'acl localnet src [IP address van alle hosts hun netwerken]' >> /etc/squid/squid.conf ${NC}${YEL}"
+echo -e "${RED}echo 'http_access allow localnet' >> /etc/squid/squid.conf ${NC}${YEL}"
+echo -e "${RED}echo 'acl forbiddensites url_regex "/etc/squid/forbiddensites"' ${NC}${YEL}"
+echo -e "${RED}echo 'http_access deny forbiddensites' >> /etc/squid/squid.conf ${NC}${YEL}"
+echo -e "${RED}echo '.facebook.com' >> /etc/squid/forbiddensites  ${NC}${YEL}"
+echo -e "${RED}firewall-cmd --permanent --add-port=3128/tcp 2>&1 ${DATA_DIR}/${LOGFILE} ${NC}${YEL}"
+echo -e "${RED}firewall-cmd --reload 2>&1 ${DATA_DIR}/${LOGFILE} ${NC}${YEL}"
+roll "Done, basic acl's made and added a forbidden list."
+echo -e "${LPURPLE}================================================================${NC}"
 }
 
 echo -e  "${BLINKPURP}###${NC} ${RED}Welcome to${NC} ${LPURPLE}Nick's${NC} ${RED}squid-proxy server roll-out script${NC}${BLINKPURP} ###${NC}${YEL}"
 roll "Please follow the upcoming instructions and insertion of variables"
 
 roll "Starting out by installing Squid!"
-
 start
 install_squid
 start_squid
+conf_squid
+restart_squid
+
