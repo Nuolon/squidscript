68e6450b4138a7db52a9665b9e7619fa5614ab28
Nuolon <Dryxonin@gmail.com>
2021-12-02 11:04:56 +0100

    Fixed hostname, colouring in terminal and correct blocklist ACL

--- main_script.sh
+++ main_script.sh
@@ -13,6 +13,7 @@ LPURPLE='\033[1;35m'
 YEL='\033[1;33m'
 BLINKRED='\033[5;31m'
 BLINKPURP='\033[5;35m'
+NRML='\033[0;37m'
 
 #Function to let text appear in a rolling-out fashion
 roll() {
@@ -32,26 +33,34 @@ roll() {
 
 #Function to declare an initial directory for environment and potentially logging (might add later)
 start() {
-	if [[ -d "$DATA_DIR" ]]; then
-		echo "" ; touch "${LOGFILE}"
-		roll "Data directory is already made, skipping..."
-	else
-		mkdir -p "${DATA_DIR}" ; touch "${LOGFILE}"
-		roll "Data directory is made, together with logfile; going next."
-	fi
-		cd "${DATA_DIR}"
+echo -e "${BLINKPURP}###${NC} ${RED}Welcome to${NC} ${LPURPLE}Nick's${NC} ${RED}httpd/apache webserver roll-out script${NC}${BLINKPURP} ###${NC}"
+echo -e "${CYAN}Please make sure you run this script as${NC}${RED} privileged user${NC}${CYAN}, are you?${NC}${YEL} [Y/N] ${NC}"
+read -p "Input: " -n 1 -r
+echo -e "${YEL}"
+if [[ $REPLY =~ ^[Nn]$  ]]
+then
+	exit 1
+fi
+	#if [[ -d "$DATA_DIR" ]]; then
+		#echo "" ; touch "${LOGFILE}"
+		#roll "Data directory is already made, skipping..."
+	#else
+		#mkdir -p "${DATA_DIR}" ; touch "${LOGFILE}"
+		#roll "Data directory is made, together with logfile; going next."
+	#fi
+		#cd "${DATA_DIR}"
 }
 
 #Function to change hostname
 change_hostname() {
-hostnamectl set-hostname Squid-proxy-server
+hostnamectl set-hostname G05-Proxy01
 }
 
 
 #Function to install squid package
 install_squid() {
 roll "Started the installation of squid, please sit tight."
-yum -y install squid* >> ${LOGFILE}
+yum -y install squid* # >> ${LOGFILE}
 roll "Installation finished, moving on..."
 echo -e "${LPURPLE}================================================================${YEL}"
 }
@@ -59,8 +68,8 @@ echo -e "${LPURPLE}=============================================================
 #Function to start squid after install and add to startup
 start_squid() {
 roll "Starting squid..."
-systemctl start squid >> ${LOGFILE}
-systemctl enable squid >> ${LOGFILE}
+systemctl start squid # >> ${LOGFILE}
+systemctl enable squid # >> ${LOGFILE}
 roll "Squid started and enabled on start-up"
 echo -e "${LPURPLE}================================================================${YEL}"
 }
@@ -68,7 +77,7 @@ echo -e "${LPURPLE}=============================================================
 #function to restart squid
 restart_squid() {
 roll "Restarting squid..."
-systemctl restart squid >> ${LOGFILE}
+systemctl restart squid # >> ${LOGFILE}
 roll "Squid restarted"
 echo -e "${LPURPLE}================================================================${YEL}"
 systemctl status squid.service
@@ -77,21 +86,25 @@ systemctl status squid.service
 #Function to change the configuration file  of squid.
 conf_squid() {
 roll "Starting to configure squid..."
-sed -i '16i http_access allow all' /etc/squid/squid.conf
-touch /etc/squid/forbiddensites
-#sed -i '53i acl forbiddensites url_regex "/etc/squid/forbiddensites"' /etc/squid/forbiddensites
+sed -i '8i acl forbidden_domains dstdomain "/etc/squid/forbidden.acl"' /etc/squid/squid.conf
+sed -i '9i http_access deny forbidden_domains' /etc/squid/forbidden.acl
+touch /etc/squid/forbidden.acl
+echo ".facebook.com" >> /etc/squid/forbidden.acl
+echo ".app.facebook.com" >> /etc/squid/forbidden.acl
+echo ".msn.com" >> /etc/squid/forbidden.acl
+echo ".bing.com" >> /etc/squid/forbidden.acl
+#sed -i '53i acl forbiddensites url_regex "/etc/squid/forbidden.acl"' /etc/squid/forbidden.acl
 #sed -i '54i http_access deny forbiddensites' /etc/squid/squid.conf
 #echo '.facebook.com' >> /etc/squid/forbiddensites
-firewall-cmd --permanent --add-port=3128/tcp 2>&1
-firewall-cmd --reload >> ${LOGFILE}
+firewall-cmd --permanent --add-port=3128/tcp # 2>&1
+firewall-cmd --reload # >> ${LOGFILE}
 roll "Done, basic acl's made and added a forbidden list."
-echo -e "${LPURPLE}================================================================${YEL}"
+echo -e "${LPURPLE}================================================================${NRML}"
 }
+#echo -e  "${BLINKPURP}###${NC} ${RED}Welcome to${NC} ${LPURPLE}Nick's${NC} ${RED}squid-proxy server roll-out script${NC}${BLINKPURP} ###${NC}${YEL}"
+#roll "Please follow the upcoming instructions and insertion of variables"
 
-echo -e  "${BLINKPURP}###${NC} ${RED}Welcome to${NC} ${LPURPLE}Nick's${NC} ${RED}squid-proxy server roll-out script${NC}${BLINKPURP} ###${NC}${YEL}"
-roll "Please follow the upcoming instructions and insertion of variables"
-
-roll "Starting out by installing Squid!"
+#roll "Starting out by installing Squid!"
 start
 install_squid
 start_squid
